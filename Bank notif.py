290cd89c670183b0da9a79faaf412d53fb5c59af
Raunaq Arora <60340725+raunaqarora21@users.noreply.github.com>
2020-05-09 20:48:26 +0530

    Add files via upload

---
+++ Bank notif.py
@@ -0,0 +1,27 @@
+n,d=list(map(int,input().split()))
+e=list(map(int,input().split()))
+x=0
+c=0
+if d%2!=0:
+    for i in  range(d,n):
+        s=e[x:x+d]
+        s.sort()
+        m=int(s[(d-1)//2])
+        #print(e[i],2*m)
+        x+=1
+        if e[i]>=(2*m):
+            c+=1
+            
+    print(c)
+else:
+    for i in  range(d,n):
+        s=e[x:x+d]
+        s.sort()
+        m=int((s[(d-1)//2])+(s[d//2]))/2
+        x+=1
+        #print(e[i],2*m)
+        if e[i]>=int(2*m):
+            c+=1
+    print(c)
+        
+
