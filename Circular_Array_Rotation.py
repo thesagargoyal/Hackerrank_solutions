66e1afd58469f85cbb8ff0555a52861694be11e0
Raunaq Arora <60340725+raunaqarora21@users.noreply.github.com>
2020-05-11 22:25:29 +0530

    Add files via upload

---
+++ Circular_Array_Rotation.py
@@ -0,0 +1,21 @@
+n,k,q = list(map(int, input().rstrip().split()))
+a = list(map(int, input().rstrip().split()))
+queries = []
+for _ in range(q):
+        queries_item = int(input())
+        queries.append(queries_item)
+
+
+while (k):
+    k-=1
+    
+    for i in range(n):
+            
+      
+        
+            
+      
+    
+            
+            
+    
