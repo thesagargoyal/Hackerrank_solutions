3a01d2f46c1389960f54e8b5715c3bc85eb22a5e
thesagargoyal <59973571+thesagargoyal@users.noreply.github.com>
2020-10-02 23:55:11 -0700

    Update Apple problem.py

--- Apple problem.py
+++ Apple problem.py
@@ -8,7 +8,7 @@ l=list(map(int,input().split(',')))
 for i in range(len(l)):
     if l[i]>i:
         print('False')
-        break
+        break           # break statement is used to break loop whenever the condition becomes true of a given block
     else:
         if l[i] == i:
             print(i)
